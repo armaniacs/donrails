43a93aa2f2168249ab88acbcfd828789274e9110
ARAKI Yasuhiro <yasuhiro@gmail.com>
2006-10-26 05:07:06 +0000

    2006-10-26  ARAKI Yasuhiro  <yasu@debian.or.jp>
            ** TABLE change. VARCHAR(100) to TEXT.
            * upgrade_1_4.mysql.sql: New file.
            * donrails/tool/tables-mysql.sql: Apply for this change. (For
            PostgreSQL and Sqlite were not modified. TODO).
    
            * donrails/app/views/notes/rdf_recent.rxml: Change channel link url from '/archives' to ''.
    
    2006-10-25  ARAKI Yasuhiro  <yasu@debian.or.jp>
    
            * donrails/tool/update-custom.sh: New file. It helps you to maintain  your layout customization. For usage, please read in this file.

--- donrails/tool/tables-mysql.sql
+++ donrails/tool/tables-mysql.sql
@@ -1,60 +1,60 @@
 CREATE TABLE pictures 
 (	
-id       SERIAL UNIQUE,
-name     VARCHAR(100),
-path      VARCHAR(100),
-size      INTEGER,
-content_type   VARCHAR(100),
-comment        TEXT,
-article_id     INTEGER,
-hidden	       INTEGER,
+id		SERIAL UNIQUE,
+name		TEXT,
+path		TEXT,
+size		INTEGER,
+content_type	VARCHAR(100),
+comment		TEXT,
+article_id	INTEGER,
+hidden		INTEGER,
 primary key (id)
 );
 
 CREATE TABLE comments (
-id       SERIAL UNIQUE,
-password    VARCHAR(100),
+id		SERIAL UNIQUE,
+password	VARCHAR(100),
 
-date     TIMESTAMP,
-title     TEXT,
-author   VARCHAR(100),
-url      VARCHAR(100),
+date		TIMESTAMP,
+title		TEXT,
+author		VARCHAR(100),
+url		TEXT,
 
-ipaddr   VARCHAR(100),
-body     TEXT,
-hidden	       INTEGER,
-article_id INTEGER,
+ipaddr		VARCHAR(100),
+body		TEXT,
+hidden		INTEGER,
+article_id	INTEGER,
 primary key (id)
 );
 
 CREATE TABLE categories (
-id       SERIAL UNIQUE,
-name         VARCHAR(100) NOT NULL UNIQUE,
-parent_id    INTEGER,
-description TEXT,
-constraint   fk_category foreign key (parent_id) references categories(id),
+id		SERIAL UNIQUE,
+name		VARCHAR(100) NOT NULL UNIQUE,
+parent_id	INTEGER,
+description	TEXT,
+constraint fk_category foreign key (parent_id) references categories(id),
 primary key (id)
 );
 
 -- enrollemnt_id added 2006-9-1(Fri)
 CREATE TABLE articles (
-id       SERIAL UNIQUE,
-title    VARCHAR(100),
-body     TEXT,
-size     INTEGER,
-article_date     TIMESTAMP,
-article_mtime    TIMESTAMP,
-hnfid    INTEGER,
-author_id  INTEGER,
-format   VARCHAR(100),
-hidden	       INTEGER,
-enrollment_id  INTEGER,
+id		SERIAL UNIQUE,
+title		TEXT,
+body		TEXT,
+size		INTEGER,
+article_date	TIMESTAMP,
+article_mtime	TIMESTAMP,
+hnfid		INTEGER,
+author_id	INTEGER,
+format		VARCHAR(100),
+hidden		INTEGER,
+enrollment_id	INTEGER,
 primary key (id)
 );
 
 CREATE TABLE categories_articles (
-category_id       INTEGER NOT NULL,
-article_id       INTEGER NOT NULL,
+category_id	INTEGER NOT NULL,
+article_id	INTEGER NOT NULL,
 constraint fk_cp_category foreign key (category_id) references categories(id),
 constraint fk_cp_article foreign key (article_id) references articles(id),
 primary key (category_id, article_id)
@@ -62,96 +62,96 @@ primary key (category_id, article_id)
 
 
 CREATE TABLE authors (
-id     SERIAL UNIQUE,
-name   VARCHAR(100) NOT NULL UNIQUE,
-nickname   VARCHAR(100),
-pass   VARCHAR(100),
-summary   TEXT,
-writable INTEGER NOT NULL,
+id		SERIAL UNIQUE,
+name		VARCHAR(100) NOT NULL UNIQUE,
+nickname	VARCHAR(100),
+pass		VARCHAR(100),
+summary		TEXT,
+writable	INTEGER NOT NULL,
 primary key (id)
 );
 
 -- CREATE TABLE blacklists (
 CREATE TABLE banlists (
-id     SERIAL UNIQUE,
-format   VARCHAR(100),
-pattern   VARCHAR(100),
-white     INTEGER,
+id		SERIAL UNIQUE,
+format		VARCHAR(100),
+pattern		TEXT,
+white		INTEGER,
 primary key (id)
 );
 
 CREATE TABLE plugins (
-id     SERIAL UNIQUE,
-name   	      VARCHAR(100) NOT NULL UNIQUE,
-description   VARCHAR(100) NOT NULL,
-manifest      VARCHAR(100) NOT NULL,
-activation    BOOLEAN,
+id		SERIAL UNIQUE,
+name		VARCHAR(100) NOT NULL UNIQUE,
+description	TEXT NOT NULL,
+manifest	VARCHAR(100) NOT NULL,
+activation	BOOLEAN,
 primary key (id)
 );
 
 CREATE TABLE blogpings (
-id          SERIAL UNIQUE,
-server_url    VARCHAR(100) NOT NULL UNIQUE,
-active	      INTEGER,
+id		SERIAL UNIQUE,
+server_url	VARCHAR(255) NOT NULL UNIQUE,
+active		INTEGER,
 primary key (id)
 );
 
 CREATE TABLE pings (
-id          SERIAL UNIQUE,
-article_id  INTEGER,
-url         VARCHAR(100) DEFAULT NULL,
-created_at  DATETIME DEFAULT NULL,
+id		SERIAL UNIQUE,
+article_id	INTEGER,
+url		TEXT DEFAULT NULL,
+created_at	DATETIME DEFAULT NULL,
 primary key (id)
 );
 
 CREATE TABLE trackbacks (
-id          SERIAL UNIQUE,
-article_id  INTEGER,
-category_id INTEGER,
-blog_name   VARCHAR(100) DEFAULT NULL,
-title       VARCHAR(100) DEFAULT NULL,
-excerpt     TEXT,
-url         VARCHAR(100) DEFAULT NULL,
-ip          VARCHAR(100) DEFAULT NULL,
-created_at  DATETIME DEFAULT NULL,
-hidden	       INTEGER,
+id		SERIAL UNIQUE,
+article_id	INTEGER,
+category_id	INTEGER,
+blog_name	TEXT DEFAULT NULL,
+title		TEXT DEFAULT NULL,
+excerpt		TEXT,
+url		TEXT DEFAULT NULL,
+ip		VARCHAR(100) DEFAULT NULL,
+created_at	DATETIME DEFAULT NULL,
+hidden		INTEGER,
 primary key (id)
 );
 
 -- added at 2006-9-1(Fri) 
 CREATE TABLE enrollments (
-id     SERIAL UNIQUE,
-title   VARCHAR(100),
-hidden	       INTEGER,
-created_at  DATETIME DEFAULT NULL,
-updated_at  DATETIME DEFAULT NULL,
+id		SERIAL UNIQUE,
+title		TEXT,
+hidden		INTEGER,
+created_at	DATETIME DEFAULT NULL,
+updated_at	DATETIME DEFAULT NULL,
 primary key (id)
 );
 
 -- added at 2006-10-14
 CREATE TABLE don_envs (
-id     SERIAL UNIQUE,
-hidden	       INTEGER,
-image_dump_path   VARCHAR(100),
-admin_user   VARCHAR(100),
-admin_password VARCHAR(100),
-admin_mailadd VARCHAR(100),
-rdf_title VARCHAR(100),
-rdf_description VARCHAR(100),
-rdf_copyright VARCHAR(100),
-rdf_managingeditor VARCHAR(100),
-rdf_webmaster VARCHAR(100),
-baseurl VARCHAR(100),
-url_limit INTEGER DEFAULT 5,
-default_theme VARCHAR(100),
+id		SERIAL UNIQUE,
+hidden		INTEGER,
+image_dump_path	TEXT,
+admin_user	VARCHAR(100),
+admin_password	VARCHAR(100),
+admin_mailadd	TEXT,
+rdf_title	TEXT,
+rdf_description TEXT,
+rdf_copyright	TEXT,
+rdf_managingeditor TEXT,
+rdf_webmaster	TEXT,
+baseurl		TEXT,
+url_limit	INTEGER DEFAULT 5,
+default_theme	VARCHAR(100),
 trackback_enable_time INTEGER,
 primary key (id)
 );
 
 -- added at 2006-10-14
 CREATE TABLE don_rbls (
-id     SERIAL UNIQUE,
-rbl_type   VARCHAR(100),
-hostname   VARCHAR(100),
+id		SERIAL UNIQUE,
+rbl_type	VARCHAR(100),
+hostname	TEXT,
 primary key (id)
 );
---
+++ donrails/tool/update-custom.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+# カスタマイズ用のシェルスクリプトをほいっと書いてみました。
+# 詳細は中をみてもらうとわかると思います。
+#
+# customというdirの下に、defaultまたはMTからの必要なファイルをコピーして、
+# DonEnv.themeをcustom としてやれば使えます。
+#
+# DONRAILS_DIR=/home/yaar/donrails-trunk
+# DERIVED=default
+#
+# だけ指定して使ってください。MT由来のならDERIVEDにはMTを書けばいいです。 
+#
+# このファイル自体は、 ~/.donrails/update-custom.sh にあると想定してます。
+# 
+# update-custom.tar.gzを展開すると、このファイルと、custom ディレクトリ以下が
+# 作成されます。
+#
+# find ~/.donrails/custom/
+# ~/.donrails/custom/
+# ~/.donrails/custom/views
+# ~/.donrails/custom/views/layouts
+# ~/.donrails/custom/views/layouts/custom
+# ~/.donrails/custom/views/layouts/custom/notes.rhtml
+#
+# Usage: sh ./$SCRIPTNAME {deploy|clean|collect|dist}"
+#
+# deployは ~/.donrails/custom/ 以下のファイルを donrailsのtreeに反映します。
+#
+# clean は donrailsのtreeにあるcustomディレクトリを削除します。
+#
+# collect は、donrailsのtree以下にある関連 fileを 
+# ~/.donrails/custom/ 以下にコピーします。
+# 手元で確認したカスタマイズ結果を集めます。
+#
+# distは必要なファイルの tar ballを作成します。
+# distを展開したらDONRAILS_DIRを設定し、 clean して deployします。
+DONRAILS_DIR=/home/yaar/donrails-trunk
+DERIVED=default
+SCRIPTNAME=update-custom.sh
+
+case "$1" in
+    deploy)
+	cd custom/views && cp -rvi * $DONRAILS_DIR/rails/app/views
+	cd $DONRAILS_DIR/rails/app/views/notes && mkdir custom && cp -viu $DERIVED/*.rhtml custom
+	cd $DONRAILS_DIR/rails/app/views/shared && mkdir custom && cp -viu $DERIVED/*.rhtml custom
+	;;
+    clean)
+	rm -rvf $DONRAILS_DIR/rails/app/views/{layouts,notes,shared}/custom
+	;;
+    collect)
+	cd custom/views/layouts/custom && cp -rviub $DONRAILS_DIR/rails/app/views/layouts/custom/*.rhtml .
+	;;
+    dist)
+	tar zcvf update-custom.tar.gz custom $SCRIPTNAME
+	;;
+    *)
+	echo "Usage: sh ./$SCRIPTNAME {deploy|clean|collect|dist}" >&2
+	exit 1
+	;;
+esac
+
+exit 0
