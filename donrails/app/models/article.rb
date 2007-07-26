1b35e76efe321a9005650faa204d452172fe7248
ARAKI Yasuhiro <yasuhiro@gmail.com>
2007-07-26 14:13:12 +0000

    2007-07-26  ARAKI Yasuhiro  <yasu@debian.or.jp>
            * donrails/app/helpers/application_helper.rb (ApplicationHelper::display
    _article_categories): Fix: link url.
    
            * donrails/app/models/article.rb (DonaCa): Fix: dupluicate category and
    article association. (issue #58)

--- donrails/app/models/article.rb
+++ donrails/app/models/article.rb
@@ -5,6 +5,7 @@ include ApplicationHelper
 class DonaCa < ActiveRecord::Base
   belongs_to :article
   belongs_to :category
+  validates_uniqueness_of :article_id, :scope => "category_id"
 end
 
 class DonaDaa < ActiveRecord::Base
