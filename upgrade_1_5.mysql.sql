85f876e20b0b204be4c60ea02ac4003911b40394
ARAKI Yasuhiro <yasuhiro@gmail.com>
2007-09-01 00:42:24 +0000

    preparing for donrails2

--- upgrade_1_5.mysql.sql
+++
@@ -1,7 +0,0 @@
-SET CHARACTER SET UTF8;
-START TRANSACTION;
-
-ALTER TABLE don_envs add column akismet_key TEXT;
-ALTER TABLE don_envs add column notify_level INTEGER;
-
-COMMIT;
--- upgrade_1_5.postgresql.sql
+++
@@ -1,7 +0,0 @@
-SET client_encoding = 'UTF8';
-START TRANSACTION;
-
-ALTER TABLE don_envs add column akismet_key VARCHAR;
-ALTER TABLE don_envs add column notify_level INTEGER;
-
-COMMIT;
